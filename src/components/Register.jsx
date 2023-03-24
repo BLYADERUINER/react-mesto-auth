527b494ed0db077e941ab542529a346c5e8887a5
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-24 23:10:22 +0400

    added/registery block styled

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Register () {
   return (
     <div className="register">
       <h2 className="register__title">Регистрация</h2>
       <form className="register__form">
-        <input className="register__input" type="email" />
-        <input className="register__input" type="password" />
+        <input className="register__input" type="email" placeholder="Email" />
+        <input className="register__input" type="password" placeholder="Пароль" />
         <button className="register__button">Зарегистрироваться</button>
+        <Link className="register__link" to="/sign-in">Уже зарегистрированы? Войти</Link>
       </form>
-      <a className="register__link" href=" ">Уже зарегистрированы? Войти</a>
     </div>
   )
 }
