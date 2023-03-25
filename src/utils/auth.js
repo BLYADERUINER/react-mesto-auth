3625a11ef433ed1e17187156ca06b0cc276c84b4
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-26 00:52:40 +0400

    added/authApi

---
+++ src/utils/auth.js
@@ -0,0 +1,42 @@
+const BASE_URL = 'https://auth.nomoreparties.co';
+
+const checkResponse = (response) => response.ok ? response.json() : Promise.reject(`Ошибка: ${response.statusText}`);
+
+export const register = ({userMail, userPassword}) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method:'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "password": userPassword,
+      "email": userMail
+    })
+  })
+  .then(checkResponse)
+};
+
+export const login = ({userMail, userPassword}) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "password": userPassword,
+      "email": userMail
+    })
+  })
+  .then(checkResponse)
+}
+
+export const checkToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    }
+  })
+  .then(checkResponse)
+};
