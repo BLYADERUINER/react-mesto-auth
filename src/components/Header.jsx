f7c845ed29567e2d6102ade3c756fa01ad4c3485
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-26 20:17:59 +0400

    added/header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import logo from '../images/header/logo.svg';
+import {Link, useLocation} from 'react-router-dom';
+
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className="header page__header">
       <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
+      {
+      location.pathname === '/sign-in' ?
+        <Link className='header__link' to="/sign-up">Регистрация</Link>
+      : <Link className='header__link' to="/sign-in">Войти</Link>
+      }
     </header>
   );
 }
