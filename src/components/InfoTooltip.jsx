bfc3320b3ff6c931730367b1c0da4ca732f2cc83
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-27 15:40:04 +0400

    fixed/pop-up info-tooltip & footer

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -1,19 +1,19 @@
+import PopupWithForm from './PopupWithForm';
 import imgTrue from '../images/pop-up_auth-reg/true.svg';
 import imgFalse from '../images/pop-up_auth-reg/false.svg'
 
 function InfoTooltip({isOpen, onClose, handleTooltip}) {
   return(
-    <div className={`pop-up pop-up__tooltip ${isOpen ? "pop-up_opened" : ""}`} >
-      <div className="pop-up__container">
-        <button className="button pop-up__button-close" type="button" onClick={onClose}></button>
-        <img className='pop-up__tooltip-image' src={handleTooltip ? imgTrue : imgFalse} alt='Картинка-подсказка' />
-        <h2 className="pop-up__tooltip-text">{
-          handleTooltip ?
-            "Вы успешно зарегистрировались!"
-          : "Что-то пошло не так! Попробуйте ещё раз."
-        }</h2>
-      </div>
-    </div>
+    <PopupWithForm popupName={'info-tooltip'} isOpen={isOpen} onClose={onClose}>
+          <img className='pop-up__tooltip-image' src={handleTooltip ? imgTrue : imgFalse} alt='Картинка-подсказка' />
+          <h2 className="pop-up__tooltip-text">
+            {
+            handleTooltip ?
+              "Вы успешно зарегистрировались!"
+            : "Что-то пошло не так! Попробуйте ещё раз."
+            }
+          </h2>
+    </PopupWithForm>
   )
 }
 
--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -3,12 +3,20 @@ function PopupWithForm({ popupName, popupTitle, popupTextButton, children, isOpe
     <div className={`pop-up pop-up_${popupName} ${isOpen ? 'pop-up_opened' : ''}`}>
       <div className="pop-up__container">
         <button className="button pop-up__button-close" type="button" onClick={onClose}></button>
-        <h2 className="pop-up__title">{`${popupTitle}`}</h2>
+        {
+        popupTitle ?
+          <h2 className="pop-up__title">{`${popupTitle}`}</h2>
+          : null
+        }
         <form className="pop-up__form" name={`${popupName}`} method="post" onSubmit={onSubmit}>
           {children}
-          <button className="button pop-up__button-save" type="submit" onChange={(evt) => evt.preventDefault()}>
-            {popupTextButton}
-          </button>
+          {
+          popupTextButton ?
+            <button className="button pop-up__button-save" type="submit" onChange={(evt) => evt.preventDefault()}>
+              {popupTextButton}
+            </button>
+            : null
+          }
         </form>
       </div>
     </div>
