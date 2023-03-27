76185a0b813666f6ba38958726153d5b57b73ec4
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-27 18:25:10 +0400

    added/pop-up loading

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,8 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRouteElement({Component, ...props}) {
   return(
-    props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />
-  )
+    props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />)
 }
 
 export default ProtectedRouteElement;
