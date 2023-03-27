e15b72387b7ca67cee069810c0ed3ca8fc1d3f7a
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-03-28 00:17:26 +0400

    added/mobile burger-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,31 @@
 import logo from '../images/header/logo.svg';
 import {Link, useLocation} from 'react-router-dom';
+import React from 'react';
 
 
 function Header({userMail, onSignout}) {
   const location = useLocation();
+  const [isToggleBurgerButton, setToggleBurgerButton] = React.useState(false);
+
+  function handleToggleBurgerButton() {
+    setToggleBurgerButton(!isToggleBurgerButton);
+  }
 
   return (
-    <header className="header page__header">
+    <header className={`header page__header ${isToggleBurgerButton ? 'header_opened' : ''}`}>
       <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
       {location.pathname === '/' ?
-        <div>
+      <>
+        <button className='header__burger-button' onClick={handleToggleBurgerButton}>
+          <span className='header__burger-span'></span>
+          <span className='header__burger-span'></span>
+          <span className='header__burger-span'></span>
+        </button>
+        <div className='header__container'>
           <span className='header__email'>{userMail}</span>
           <Link className='header__link' style={{color:'#A9A9A9'}} onClick={onSignout}>Выйти</Link>
         </div>
+      </>
       : location.pathname === '/sign-in' ?
         <Link className='header__link' to="/sign-up">Регистрация</Link>
       : <Link className='header__link' to="/sign-in">Войти</Link>
